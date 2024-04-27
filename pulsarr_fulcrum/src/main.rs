f19420bb98af8815bf674c37ef9c12547ab0e7b1
Asher Carlow <jhaldenone@gmail.com>
2024-04-27 12:18:57 -0500

    Working docker deploys and db connections (#3)
    * added db connection pool and migrations
    
    * working in docker (kinda)
    
    * working in docker

--- pulsarr_fulcrum/src/main.rs
+++ pulsarr_fulcrum/src/main.rs
@@ -1,14 +1,25 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
+use sqlx::postgres::PgPool;
+use std::env;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), sqlx::Error> {
+    // setup db connection and run any necessary migrations
+    println!("app starting");
+    let db_url = env!("DATABASE_URL");
+    println!("connecting to db: {db_url}");
+    let pool = PgPool::connect(&db_url).await?;
+    println!("running migrations");
+    sqlx::migrate!("db/migrations").run(&pool).await?;
+    println!("migrations complete");
+
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Welcome to Pulsarr" }));
 
-    // run our app with hyper, listening globally on port 3000 TODO: find a way to make the port environment dependent
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3003").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    // run our app with hyper, listening globally on port 3003
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3003").await?;
+    println!("serving on 3003");
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
