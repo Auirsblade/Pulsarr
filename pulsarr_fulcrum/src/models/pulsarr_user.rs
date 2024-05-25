ab85d729028ac1b9605e779e1cc481ce5e941bfd
Asher Carlow <jhaldenone@gmail.com>
2024-05-24 22:54:59 -0500

    adding post endpoints for adding user and group

--- pulsarr_fulcrum/src/models/pulsarr_user.rs
+++ pulsarr_fulcrum/src/models/pulsarr_user.rs
@@ -1,12 +1,14 @@
-use rocket::{get, State};
-use rocket::serde::{Deserialize, Serialize};
+use std::result;
+use crate::error::PulsarrError;
 use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+use rocket::{get, post, State};
 use rocket_okapi::{
-    JsonSchema, okapi::openapi3::OpenApi, openapi, openapi_get_routes_spec,
-    settings::OpenApiSettings,
+    okapi::openapi3::OpenApi, openapi, openapi_get_routes_spec, settings::OpenApiSettings,
+    JsonSchema,
 };
 use sqlx;
-use sqlx::FromRow;
+use sqlx::{FromRow, PgPool};
 
 use crate::PostgresState;
 
@@ -18,7 +20,7 @@ struct PulsarrUser {
 
 /// Api Logic
 pub fn get_routes_and_docs(settings: &OpenApiSettings) -> (Vec<rocket::Route>, OpenApi) {
-    openapi_get_routes_spec![settings: get_pulsarr_user]
+    openapi_get_routes_spec![settings: get_pulsarr_user, add_user]
 }
 
 /// # Get a user by id
@@ -36,4 +38,36 @@ async fn get_pulsarr_user(
             .unwrap();
 
     Ok(Json(rating_system))
+}
+
+/// # Add a user
+#[openapi(tag = "User")]
+#[post("/", format = "application/json", data = "<user>")]
+async fn add_user(
+    state: &State<PostgresState>,
+    user: Json<PulsarrUser>,
+) -> crate::PulsarrResult<bool> {
+    match new_save(user.into_inner(), &state.pool).await {
+        (true, _) => Ok(Json(true)),
+        (false, error_message) => Err(PulsarrError {
+            err: "validation error".to_owned(),
+            msg: error_message,
+            http_status_code: 400,
+        }),
+    }
+}
+
+async fn new_save(user: PulsarrUser, pool: &PgPool) -> (bool, Option<String>) {
+    let result = sqlx::query(
+        "INSERT INTO pulsarr_user (name)\
+        VALUES ($1)",
+    )
+    .bind(user.name)
+    .execute(pool)
+    .await;
+
+    match result { 
+        Ok(_) => (true, None),
+        Err(err) => (false, Some(err.to_string()))
+    }
 }
