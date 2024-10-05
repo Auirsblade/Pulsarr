17684e2b7308ac7184cba3974416ad3289405846
Jude Hoogendyk <j.m.hoogendyk@gmail.com>
2024-10-05 16:29:31 -0500

    added shell project

---
+++ PulsarrApp/PulsarrAppTests/PulsarrAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PulsarrAppTests.swift
+//  PulsarrAppTests
+//
+//  Created by Jude Hoogendyk on 10/5/24.
+//
+
+import Testing
+
+struct PulsarrAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
